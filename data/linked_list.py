70269ad3cfac8a77d9ff16fc38a3776054890c9f
tiennham <hongtiennham@gmail.com>
2024-09-06 17:35:09 +0700

    generate linked list object

---
+++ data/linked_list.py
@@ -0,0 +1,38 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    @staticmethod
+    def build_linked_list_from_list(values):
+        node_lst = []
+        for i in values:
+            node_lst.append(ListNode(i))
+
+        for i in range(len(node_lst)):
+            node_lst[i].next = node_lst[i+1] if i+1 < len(node_lst) else None
+
+        return node_lst[0]
+
+    @staticmethod
+    def build_cycle_linked_list(values, pos):
+        if len(values) < 2:
+            print("length must be greater than 2")
+            return None
+        node_lst = []
+        for i in values:
+            node_lst.append(ListNode(i))
+
+        for i in range(len(node_lst)):
+            if i + 1 < len(node_lst):
+                node_lst[i].next = node_lst[i + 1]
+
+        node_lst[-1].next = node_lst[pos]
+
+        return node_lst[0]
+
+
+
+
+
+
