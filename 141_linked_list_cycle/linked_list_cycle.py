e846eea2524e7148a4ac489bbdf86893e1e4231e
tiennham <hongtiennham@gmail.com>
2024-09-07 09:01:31 +0700

    linked list cycle: slow = slow.next, fast = fast.next.next. If fast == slow: is_cycle == True

---
+++ 141_linked_list_cycle/linked_list_cycle.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow_node = head
+        fast_node = head
+        while fast_node and fast_node.next:
+            slow_node = slow_node.next
+            fast_node = fast_node.next.next
+
+            if slow_node == fast_node:
+                return True
+
+        return False
+
+    def my_has_cycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        checked_nodes = []
+        current = head.next
+        while current:
+            if current in checked_nodes:
+                return True
+            checked_nodes.append(current)
+            current = current.next
+
+        return False
+
+    
